56f9c5f10e869427b5328a4496bf0502b750a350
Alena <alena.nicolae@gmail.com>
2018-04-25 10:47:50 -0700

    First commit

--- js/app.js
+++ js/app.js
@@ -24,7 +24,19 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Player {
 
+  update() {
+
+  }
+
+  render() {
+    
+  }
+
+}
+
+const player = new Player();
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
@@ -44,3 +56,5 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+const allEnemies = [];
